485cd9afa9fead934b36449fc7c00afc2133cbfb
Martin Cornejo <martincckd130@gmail.com>
2023-10-19 01:26:07 -0300

    Ejercicio 3 finalizado

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// EJERCICIO 3 - CONVERSIÓN DE GRADOS A CELSIUS FAHRENHEIT 
+
+var gradosCelsius = 25;
+
+var gradosFahrenheit = (gradosCelsius * 9/5) + 32;
+
+console.log("Grados Celsius: " + gradosCelsius);
+console.log("Grados Fahrenheit: " + gradosFahrenheit);
