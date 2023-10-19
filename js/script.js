3dc2cc3510d524317f23c6b30a8ac7e8596d0066
Martin Cornejo <martincckd130@gmail.com>
2023-10-19 01:14:47 -0300

    Finalizado el ejercicio 1

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// EJERCICIO 1 - VARIABLES Y OPERACIONES MATEMATICAS
+
+let numero1 = 50;
+let numero2 = 10;
+
+console.log("Resultado de la suma:", numero1 + numero2);
+console.log("Resultado de la resta:", numero1 - numero2);
+console.log("Resultado de la multiplicación:", numero1 * numero2);
+console.log("Resultado de la división:", numero1 / numero2);
+
