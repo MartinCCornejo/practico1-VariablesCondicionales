f20a6ea4a05c6fbc0c6ca4a69271dfbd21673484
Martin Cornejo <martincckd130@gmail.com>
2023-10-22 13:27:16 -0300

    Ejercicios del 1 al 5 realizados

---
+++ js/TrabajoPractico-1/ejercicio-4.js
@@ -0,0 +1,5 @@
+// EJERCICIO 4 
+// 4.- Escribe un programa de dos líneas que pida el nombre del usuario con un prompt y escriba un texto que diga «Hola nombreUsuario»
+
+nombreUsuario = prompt("Escriba su nombre de usuario");
+document.write("Hola " + nombreUsuario);
---
+++ js/TrabajoPractico-1/ejercicio-5.js
@@ -0,0 +1,6 @@
+// TRABAJO PRACTICO 5 
+// 5.- Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.
+
+numero1 = parseInt(prompt("Escriba el primer número"));
+numero2 = parseInt(prompt("Escriba el segundo número"));
+document.write("El resultado es:", numero1 + numero2);
